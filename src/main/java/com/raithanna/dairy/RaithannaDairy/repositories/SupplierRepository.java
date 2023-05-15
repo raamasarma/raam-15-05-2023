988ce8f428d576635659dc6c002ef8c36ddaab3e
raam <ramasarma.pakala@gmail.com>
2023-05-15 09:30:45 +0530

    first commit

---
+++ src/main/java/com/raithanna/dairy/RaithannaDairy/repositories/SupplierRepository.java
@@ -0,0 +1,14 @@
+package com.raithanna.dairy.RaithannaDairy.repositories;
+
+import com.azure.spring.data.cosmos.repository.Query;
+import com.raithanna.dairy.RaithannaDairy.models.supplier;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface SupplierRepository extends CrudRepository<supplier,Integer> {
+   List<supplier> findByOrderByIdDesc();
+   supplier findBySupplierCode(String supplierCode);
+   @Query("select * from  supplier where supplierName=?1 ")
+   supplier findBySupplierName(String supplierName);
+}
